63f1271b13fefcf4aa405d119bca80d7ea7c909a
Uday Bhanu <bhanusharm122@gmail.com>
2024-02-16 01:03:07 +0530

    UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
-import {IoSearchOutline} from "react-icons/io5";
-import {CiUser} from "react-icons/ci";
-import {IoMdStarOutline} from "react-icons/io";
+
 import Navbar from "@/components/Navbar";
 import Banner from "@/components/Banner";
 import Contact from "@/components/Contact";
